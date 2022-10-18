7e174db72be6b92b6c3ae8b5be080219b8660c73
Siripa-Maneein <siripa.m@ku.th>
2022-10-18 23:22:42 +0700

    Remove unnecessary import

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponseRedirect
 
 urlpatterns = [
     path('', include('aquaholic.urls')),

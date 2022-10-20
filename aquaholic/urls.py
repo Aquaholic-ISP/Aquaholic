344a2373636c118c273bc1ff1e07efcfe2b6a968
Siripa-Maneein <siripa.m@ku.th>
2022-10-20 17:46:55 +0700

    Create view for calculate button in homepage

--- aquaholic/urls.py
+++ aquaholic/urls.py
@@ -8,6 +8,7 @@ app_name="aquaholic"
 urlpatterns = [
     path('aquaholic/', views.HomePage.as_view(), name='home'),
     path("", RedirectView.as_view(url="/aquaholic/")),
+    path('aquaholic/calculate', views.calculate, name='calculate'),
     path('aquaholic/<int:pk>/set_up', views.SetUp.as_view(), name='set_up'),
     path('aquaholic/<int:pk>/progress', views.Progress.as_view(), name='progress'),
     path('aquaholic/<int:pk>/input', views.Input.as_view(), name='input'),
--- aquaholic/views.py
+++ aquaholic/views.py
@@ -1,11 +1,22 @@
 from django.views import generic
 from .models import UserInfo, Schedule, Intake
+from django.shortcuts import render
+from .models import UserInfo, KILOGRAM_TO_POUND, OUNCES_TO_MILLILITER
 
 
 class HomePage(generic.ListView):
     """A class that represents the home page view."""
     model = UserInfo
     template_name = 'aquaholic/home.html'
+
+
+def calculate(request):
+    if request.method == "POST":
+        weight = float(request.POST["weight"])
+        exercise_time = float(request.POST["exercise_time"])
+        water_amount_per_day = ((weight * KILOGRAM_TO_POUND * 0.5)
+                                + (exercise_time / 30) * 12) * OUNCES_TO_MILLILITER
+        return render(request, 'aquaholic/home.html', {'result': f"{water_amount_per_day:.2f}"})
 
 
 class SetUp(generic.DetailView):

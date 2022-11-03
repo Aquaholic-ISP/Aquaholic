3b3fb5ecdc4f8a69b6a3d8b4668a36c5e79d0b5e
jindasy <jindaporn.sy@gmail.com>
2022-11-03 23:43:35 +0700

    add input view

--- aquaholic/views.py
+++ aquaholic/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, reverse, redirect
 from .models import UserInfo, KILOGRAM_TO_POUND, OUNCES_TO_MILLILITER
 from .notification import get_access_token, send_notification
 from django.http import HttpResponseRedirect
+from django.utils.timezone import make_aware
 import datetime
 
 
@@ -167,6 +168,30 @@ class Input(generic.DetailView):
     """A class that represents the input page view."""
     model = Intake
     template_name = 'aquaholic/input.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'user': request.user})
+
+    def post(self, request, *args, **kwargs):
+        try:
+            amount = request.POST["amount"]
+            date = request.POST["date"]
+            intake_dated = datetime.datetime.strptime(date, '%Y-%m-%d')
+            aware_date = make_aware(intake_dated)
+            userinfo = UserInfo.objects.get(user_id=request.user.id)
+            if Intake.objects.filter(user_info_id=userinfo.id, intake_date=aware_date).exists():
+                intake = Intake.objects.get(user_info_id=userinfo.id, intake_date=aware_date)
+                intake.user_drinks_amount += float(amount)
+                intake.save()
+            else:
+                Intake.objects.create(user_info_id=userinfo.id,
+                                      intake_date=date,
+                                      user_drinks_amount=amount)
+            return HttpResponseRedirect(reverse('aquaholic:home'))
+        except:
+            message = "Please, input in the field."
+            return render(request, self.template_name,
+                          {'message': message})
 
 
 class History(generic.DetailView):

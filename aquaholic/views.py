53de7104e1f67b9e886e1f35079b29e561586cb0
Siripa-Maneein <siripa.m@ku.th>
2022-11-24 00:03:12 +0700

    fix timezone warning in cron job view

--- aquaholic/views.py
+++ aquaholic/views.py
@@ -426,7 +426,7 @@ def update_notification(request):
     in a user's schedule is sent, the time in the schedule will be
     added by 24 hours.
     """
-    all_to_send = Schedule.objects.filter(notification_time__lte=datetime.datetime.now(),
+    all_to_send = Schedule.objects.filter(notification_time__lte=make_aware(datetime.datetime.now()),
                                           notification_status=False)
     for one_to_send in all_to_send:
         send_notification(f"Don't forget to drink {one_to_send.expected_amount} ml of water",
@@ -435,7 +435,7 @@ def update_notification(request):
         one_to_send.save()
 
     last_to_send = Schedule.objects.filter(notification_status=True, is_last=True,
-                                           notification_time__lte=datetime.datetime.now())
+                                           notification_time__lte=make_aware(datetime.datetime.now()))
     for last_schedule in last_to_send:
         user_info = last_schedule.user_info
         user_schedule = Schedule.objects.filter(user_info=user_info)
